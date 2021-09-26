8ed3a24acda828d7756104cd7b17f4c25d84d0f7
Anna Khuseinova <annakhuseinova@yandex.ru>
2021-09-26 14:17:00 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/jasperreportsspringboot/JasperreportsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.jasperreportsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperreportsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasperreportsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/jasperreportsspringboot/JasperreportsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.jasperreportsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperreportsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
